13a69b887aca4417199c513596b81da0804c5cbd
Eirik Ovesen <eirikovs123@hotmail.no>
2023-12-09 00:57:15 +0100

    solve day 2 task 2

--- day2/algo.py
+++ day2/algo.py
@@ -1,28 +1,28 @@
 
 
+# Main algo
+file = open('input.txt')
+
+lines = file.readlines()
+
+
+# Task 1
 CUBES = {
     "red": "12",
     "green": "13",
     "blue": "14"
 }
 
-# Main algo
-file = open('input.txt')
-
-lines = file.readlines()
-
-def join_all_picks(string):
-    delimiters = [",", ";"]
+def join_all_picks(string, delimiters):
     for delimiter in delimiters:
         string = "".join(string.split(delimiter))
     
     return string
 
-# Task 1
 def task_one_game_is_possible(line): 
     game_is_possible = True
     line_without_game_id = line.split(":")[-1]
-    all_picks = join_all_picks(line_without_game_id)
+    all_picks = join_all_picks(line_without_game_id, [",", ";"])
 
     # Replace all colors with numbers
     for key, value in CUBES.items():
@@ -41,35 +41,56 @@ def task_one_game_is_possible(line):
     
     return game_is_possible
 
-# The line counter corresponds to the game ID
-task_one_line_counter = 1
-task_one_sum_of_possible_game_ids = 0
-impossible_games_counter = 0
-impossible_game_ids = []
+def task_one():
+    # The line counter corresponds to the game ID
+    task_one_line_counter = 1
+    task_one_sum_of_possible_game_ids = 0
+    impossible_games_counter = 0
+    impossible_game_ids = []
 
-for line in lines:
-    if task_one_game_is_possible(line) is True:
-        task_one_sum_of_possible_game_ids += task_one_line_counter
-        impossible_games_counter += 1
-        impossible_game_ids.append(task_one_line_counter)
+    for line in lines:
+        if task_one_game_is_possible(line) is True:
+            task_one_sum_of_possible_game_ids += task_one_line_counter
+            impossible_games_counter += 1
+            impossible_game_ids.append(task_one_line_counter)
+            
+        task_one_line_counter += 1
+            
+    print(task_one_sum_of_possible_game_ids)
+    print(impossible_games_counter)
+    print(impossible_game_ids)
+
+def task_two():
+    total_sum_of_powers = 0
+    
+    for line in lines:
+        power_of_sets = 0
+        line_without_game_id = line.split(":")[-1].strip()
+        current_lowest_picks = {
+            "blue": 0,
+            "red": 0,
+            "green": 0
+        }
+
+        all_picks = line_without_game_id.replace(";", ",")
+
+        all_picks_as_array = all_picks.split(", ")
+
+        for pick in all_picks_as_array:
+            number, color = pick.split(" ")
+            for color_name, lowest in current_lowest_picks.items():
+                if color == color_name and int(number) > lowest:
+                    current_lowest_picks[color] = int(number)
+
+        power_of_current_lowest_picks = 1
+        for color_name, lowest in current_lowest_picks.items():
+            power_of_current_lowest_picks *= lowest
         
-    task_one_line_counter += 1
-        
-print(task_one_sum_of_possible_game_ids)
-print(impossible_games_counter)
-print(impossible_game_ids)
+        total_sum_of_powers += power_of_current_lowest_picks
+    
+    print(total_sum_of_powers)
 
-# Task 2
-# def task_two_game_is_possible(line):
-#     return True
 
-# task_two_line_counter = 1
-# task_two_sum_of_possible_game_ids = 0
+# task_one()
 
-# for line in lines:
-#     if game_is_possible(line):
-#         task_two_sum_of_possible_game_ids += task_two_line_counter
-        
-#     task_two_line_counter += 1
-
-# print(task_two_sum_of_possible_game_ids)
+task_two()
